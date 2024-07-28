a2f298d934d0c3fb0f4834ded55cc18a9d3177b7
Egaxyz <egagunawannovaldi@gmail.com>
2024-07-28 19:24:04 +0700

    lier

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,2 +1,5 @@
 <template>
+  <div class="flex flex-row-reverse space-x-4 space-x-reverse">
+    <p class="underline decoration-wavy">rawwrrr</p>
+  </div>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/layouts/AdminLayout.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
